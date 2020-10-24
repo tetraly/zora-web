2aba0e9ab71b11f45741788666b657969004efc1
tetraly <tetraleigh@gmail.com>
2020-10-24 17:44:37 -0400

    add test settings

--- zora_web/settings.py
+++ zora_web/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = local.SECRET_KEY
+SECRET_KEY = "please-change-this"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = local.DEBUG
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com'] 
+ALLOWED_HOSTS = ['*'] 
 
 # Application definition
 
