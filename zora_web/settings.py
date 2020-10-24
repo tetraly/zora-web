99c1a273b689181f82f4585e88e29cda433ad7fe
tetraly <tetraleigh@gmail.com>
2020-10-24 19:23:16 -0400

    add digitalocean app to allowlist

--- zora_web/settings.py
+++ zora_web/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,zora-web-cnhqv.ondigitalocean.app").split(",")
 # Application definition
 
 INSTALLED_APPS = [
